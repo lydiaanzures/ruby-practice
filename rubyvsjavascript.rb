a2bfe1f968118a3d9a5e256b0e5163a043dc09d8
Lydia Anzures <lydiaanzures@gmail.com>
2021-12-01 23:57:14 -0500

    is palindrome prompt

--- rubyvsjavascript.rb
+++ rubyvsjavascript.rb
@@ -205,6 +205,18 @@ end
 
 searchArray([1,2,3],3)
 
+# // Write a method that checks whether or not a string is a palindrome. Here is the javascript:
+
+# const isPalindrome = (str) => {
+#   for(let i = 0; i < str.length/2; i++){
+#     if(str[i] != str[str.length-i-1]){
+#       return false;
+#       break;
+#     }
+#   }
+#   return true;
+# };
+
 
 
 
