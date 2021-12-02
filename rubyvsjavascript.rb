f2057175f2c665866afcad5d385d33095fa4431d
Lydia Anzures <lydia@Lydias-MacBook-Pro.local>
2021-12-01 21:38:53 -0500

    First function

---
+++ rubyvsjavascript.rb
@@ -0,0 +1,18 @@
+# comparing Ruby against Javascript!
+
+# method that accepts a name from the use and then returns it
+
+# javascript would look like this
+ 
+# const getName = () => {
+#     let name = prompt("What is your name?");
+#     return name;
+# };
+
+# Ruby
+
+def hello_you (you)
+    puts "Hello! #{you}"
+end
+
+p hello_you(1)
