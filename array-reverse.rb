8404f5723939ba1fc2550ab940d698e776060d35
Lydia Anzures <lydiaanzures@gmail.com>
2021-12-04 12:50:57 -0500

    odd-even

---
+++ array-reverse.rb
@@ -0,0 +1,12 @@
+
+string ="Hello"
+
+# reverses string backwards
+p string.reverse
+
+array = ["Lydia", "Henry"]
+# reverses order or elements in array, but not strings themeselves
+p array.reverse
+
+
+
---
+++ odd-even.rb
@@ -0,0 +1,21 @@
+# odd and even methods
+
+# Ruby program of integer odd? function
+  
+# Initializing the numbers 
+num1 = 19
+num2 = 2
+num3 = 28
+num4 = 13
+   
+# Prints true if number is odd
+puts num1.odd? 
+puts num2.odd?
+puts num3.odd? 
+puts num4.odd?
+
+# Prints true if number is even
+puts num1.even? 
+puts num2.even?
+puts num3.even? 
+puts num4.even?
