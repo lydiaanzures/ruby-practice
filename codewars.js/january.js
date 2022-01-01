474a17d212f3bb1f023f345bb11a9b4b5d2e380e
Lydia Anzures <lydiaanzures@gmail.com>
2022-01-01 13:24:10 -0500

    start of croquet club

--- codewars.js/january.js
+++ codewars.js/january.js
@@ -2,4 +2,24 @@ function multiply(a, b){
     return a * b
   }
 
-  console.log(multiply(9,10))
+console.log(multiply(9,10))
+
+
+// /////////////////////////////////////////////
+
+function openOrSenior(data){
+    console.log(data.length)
+// senior = 55+ and handicap 7+
+    for (i=0; i<data.length; i++){
+        console.log("here")
+        if (i[0] > 55){
+            console.log("OLDDD")
+        }
+    return data
+    }
+
+        
+}
+
+openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]])
+console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]))
