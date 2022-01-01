58b5da9cab0699e4fda309a393bc8cf20cf5102c
Lydia Anzures <lydiaanzures@gmail.com>
2022-01-01 13:32:37 -0500

    open or senior function

--- codewars.js/january.js
+++ codewars.js/january.js
@@ -6,20 +6,25 @@ console.log(multiply(9,10))
 
 
 // /////////////////////////////////////////////
-
 function openOrSenior(data){
-    console.log(data.length)
-// senior = 55+ and handicap 7+
+// senior = 55+ and handicap 7+, otherwise, normal member
+    output = []
     for (i=0; i<data.length; i++){
-        console.log("here")
-        if (i[0] > 55){
-            console.log("OLDDD")
+        const age = data[i][0]
+        const handicap = data[i][1]
+        if (age >= 55 && handicap >= 7){
+            const status = "Senior"
+            output.push(status)
         }
-    return data
+        else{
+            const status = "Open"
+            output.push(status)
+            }
+        }
+    console.log(output)
     }
 
         
-}
+
 
 openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]])
-console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]))
