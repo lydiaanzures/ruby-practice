74bd208fe6d46d97ed4c917f0aed720a8bff8a81
Lydia Anzures <lydiaanzures@gmail.com>
2022-01-03 01:27:58 -0500

    Serra, Brazil

--- carmen-sandiego/find_carmen.sql
+++ carmen-sandiego/find_carmen.sql
@@ -128,23 +128,32 @@ lydia=# SELECT language FROM countrylanguage WHERE countrycode = 'SMR';
 
 -- San Marino, check the cities
 lydia=# SELECT * FROM city WHERE city.countrycode = 'SMR';
-  id  |    name    | countrycode |     district      | population
-------+------------+-------------+-------------------+------------
- 3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
- 3171 | San Marino | SMR         | San Marino        |       2294
-(2 rows)
--- Omitting the obvious city, left with Serravalle
-
--- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-lydia=# SELECT * FROM city WHERE city.countrycode = 'SMR';
 --   id  |    name    | countrycode |     district      | population
 -- ------+------------+-------------+-------------------+------------
 --  3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
 --  3171 | San Marino | SMR         | San Marino        |       2294
 -- (2 rows)
+-- Omitting the obvious city, left with Serravalle
 
-Serravalle 
+-- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+lydia=# SELECT * FROM city WHERE city.name LIKE 'Serr%';
+--   id  |    name    | countrycode |     district      | population
+-- ------+------------+-------------+-------------------+------------
+--   265 | Serra      | BRA         | Esp�rito Santo  |     302666
+--  3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
+-- (2 rows)
 
+
+-- Serra, which is in 'BRA'
+-- Use country code to search country name
+lydia=# SELECT * FROM country JOIN city on country.code = city.countrycode WHERE city.name = 'Serra';
+--  code |  name  |   continent   |    region     | surfacearea  | indepyear | population | lifeexpectancy |    gnp    |  gnpold   | localname |  governmentform  |        headofstate        | capital | code2 | id  | name  | countrycode |     district     | population
+-- ------+--------+---------------+---------------+--------------+-----------+------------+----------------+-----------+-----------+-----------+------------------+---------------------------+---------+-------+-----+-------+-------------+------------------+------------
+--  BRA  | Brazil | South America | South America | 8.547403e+06 |      1822 |  170115000 |           62.9 | 776739.00 | 804108.00 | Brasil    | Federal Republic | Fernando Henrique Cardoso |     211 | BR    | 265 | Serra | BRA         | Esp�rito Santo |     302666
+-- (1 row)
+
+
+-- Serra, Brazil
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
