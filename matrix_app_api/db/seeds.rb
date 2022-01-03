14655078dac284bdbec6e323988cbcf6fa67c5c8
Lydia Anzures <lydiaanzures@gmail.com>
2022-01-03 00:07:49 -0500

    seed database commit 7

--- matrix_app_api/db/seeds.rb
+++ matrix_app_api/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Model.create({ column: data, column: data })
+
+Character.create([
+	{ name: "Neo", description: false },
+	{ name: "Trinity", description: false },
+	{ name: "Morpheus", description: false },
+	{ name: "Agent Smith", description: false },
+])
