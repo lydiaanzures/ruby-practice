ed6db20bd02df079770b61b9b3110358d4ab61d2
Lydia Anzures <lydiaanzures@gmail.com>
2022-01-11 21:33:20 -0500

    isprime function

--- codewars/january.js
+++ codewars/january.js
@@ -107,4 +107,17 @@ sumTwoSmallestNumbers([52, 76, 14, 12, 4])
 
 
 
+function isPrime(num) {
+    // Edge case
+    if (n <= 1)
+    return false;
 
+    // Check from 2 to n-1
+    for (let i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+
+    return true;
+  }
+  function isPrime(n)
+  
