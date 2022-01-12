44b14d42dcb6626d7bf23045191ee981cfd67cfa
Lydia Anzures <lydiaanzures@gmail.com>
2022-01-11 22:34:21 -0500

    findOdd

--- codewars/january.js
+++ codewars/january.js
@@ -119,5 +119,28 @@ function isPrime(num) {
 
     return true;
   }
-  function isPrime(n)
-  
+
+
+
+// Given an array of integers, find the one that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+
+function findOdd(A) {
+        //loop thorugh array, create a sub array
+        for (let i = 0; i < A.length; i++){
+            count = A.filter(number => number == A[i]).length
+            if (count % 2 == 1) {
+                // console.log(A[i])
+                return A[i];
+            }
+        }
+    return 0;
+  }
+
