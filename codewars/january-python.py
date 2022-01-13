487993795bf1e04d7ce52c5c39406d932146f20f
Lydia Anzures <lydiaanzures@gmail.com>
2022-01-12 21:22:24 -0500

    basic_op

--- codewars/january-python.py
+++ codewars/january-python.py
@@ -2,4 +2,23 @@ def find_smallest_int(arr):
     arr.sort()
     return arr[0]
 
+def positive_sum(arr):
+    running_sum = 0
+    for num in arr:
+        if num >= 0:
+            running_sum += num
+    return running_sum
+
+def basic_op(operator, value1, value2):
+    if operator == '+':
+        result = value1 + value2
+    if operator == '-':
+        result = value1 - value2
+    if operator == '*':
+        result = value1 * value2
+    if operator == '/':
+        result = value1 / value2
+    return result
+
+
 
